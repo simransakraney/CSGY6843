08f8453996128af2c01483c9c81a413d5ccb1d01
simransakraney <ss21489@nyu.edu>
2026-01-27 22:27:40 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -33,7 +33,7 @@ def welcome_assignment_answers(question):
 
 
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-        answer = 2
+        answer = 4
 
     elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
         answer = 3
@@ -59,6 +59,7 @@ def welcome_assignment_answers(question):
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
 
 
 
